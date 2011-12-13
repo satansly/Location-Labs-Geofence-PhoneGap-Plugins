f8a30c24f59615f091752753f9c1aaacdace3e1f
satansly <satansly@gmail.com>
2011-12-13 05:17:35 +0500

    Domain changed

--- Android/src/com/precisosol/llgeofence/GeofencePlugin.java
+++ Android/src/com/precisosol/llgeofence/GeofencePlugin.java
@@ -22,7 +22,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
-package com.sortaprecisiontech.llgeofence;
+package com.precisosol.llgeofence;
 
 import java.util.ArrayList;
 import java.util.Map;
